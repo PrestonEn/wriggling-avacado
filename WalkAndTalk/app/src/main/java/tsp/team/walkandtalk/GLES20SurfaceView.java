614aeadd780d8a83ffc90b43913283e886c02963
DisneyLover <stace.v.14@gmail.com>
2015-12-08 12:02:31 -0500

    Damage Control 1

---
+++ WalkAndTalk/app/src/main/java/tsp/team/walkandtalk/GLES20SurfaceView.java
@@ -0,0 +1,76 @@
+package tsp.team.walkandtalk;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.Toast;
+
+/**
+ * Created by preston on 15-11-28.
+ */
+public class GLES20SurfaceView extends GLSurfaceView{
+
+
+    private final GLES20Renderer mRenderer;
+
+    public GLES20SurfaceView(Context context) {
+        super(context);
+
+        // Create an OpenGL ES 2.0 context.
+        setEGLContextClientVersion(2);
+
+        // Set the Renderer for drawing on the GLSurfaceView
+        mRenderer = new GLES20Renderer();
+        setRenderer(mRenderer);
+
+        // Render the view only when there is a change in the drawing data
+        setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+    }
+
+
+    private final float TOUCH_SCALE_FACTOR = 180.0f / 320;
+    private float mPreviousX;
+    private float mPreviousY;
+
+    @Override
+    public boolean onTouchEvent(MotionEvent e) {
+
+        // MotionEvent reports input details from the touch screen
+        // and other input controls. In this case, you are only
+        // interested in events where the touch position changed.
+        Log.v("EVENT", new Float(e.getRawX()).toString());
+
+        Log.v("width", new Integer(getWidth()).toString());
+        int height = getHeight();
+
+        float x = e.getX();
+        float y = e.getY();
+
+        switch (e.getAction()) {
+            case MotionEvent.ACTION_MOVE:
+
+                float dx = x - mPreviousX;
+                float dy = y - mPreviousY;
+
+                // reverse direction of rotation above the mid-line
+                if (y > getHeight() / 2) {
+                    dx = dx * -1 ;
+                }
+
+                // reverse direction of rotation to left of the mid-line
+                if (x < getWidth() / 2) {
+                    dy = dy * -1 ;
+                }
+
+                mRenderer.setAngle(
+                        mRenderer.getAngle() +
+                                ((dx + dy) * TOUCH_SCALE_FACTOR));  // = 180.0f / 320
+                requestRender();
+        }
+
+        mPreviousX = x;
+        mPreviousY = y;
+        return true;
+    }
+}
--- WalkAndTalk/app/src/main/java/tsp/team/walkandtalk/GameActivity.java
+++ WalkAndTalk/app/src/main/java/tsp/team/walkandtalk/GameActivity.java
@@ -1,6 +1,8 @@
 package tsp.team.walkandtalk;
 
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 
 /**
@@ -8,10 +10,38 @@ import android.os.Bundle;
  */
 public class GameActivity extends Activity {
 
+    private GLSurfaceView mGLView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+        //this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        // Create a GLSurfaceView instance and set it
+        // as the ContentView for this Activity
+        mGLView = new GLES20SurfaceView(this);
+        setContentView(mGLView);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // The following call pauses the rendering thread.
+        // If your OpenGL application is memory intensive,
+        // you should consider de-allocating objects that
+        // consume significant memory here.
+        mGLView.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // The following call resumes a paused rendering thread.
+        // If you de-allocated graphic objects for onPause()
+        // this is a good place to re-allocate them.
+        mGLView.onResume();
     }
 }

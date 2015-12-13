4cc109c627631a4863874e2f88f3cdf5e9f2f14e
Tyler Collins <advent14@gmail.com>
2015-12-13 15:41:48 -0500

    Got a basic still enemy moving on the screen.
    EnemyFactory has also been started so that's good. Moving on from here.

---
+++ WalkAndTalk/app/src/main/java/tsp/team/walkandtalk/DifficultySetting.java
@@ -0,0 +1,10 @@
+package tsp.team.walkandtalk;
+
+/**
+ * Enumeration type used in defining enemy speed and direction.
+ */
+public enum DifficultySetting {
+    DIFFICULTY_HARD,
+    DIFFICULTY_MEDIUM,
+    DIFFICULTY_EASY,
+}
--- WalkAndTalk/app/src/main/java/tsp/team/walkandtalk/Square.java
+++ WalkAndTalk/app/src/main/java/tsp/team/walkandtalk/Square.java
@@ -57,11 +57,7 @@ public class Square extends Sprite{
                 0.0f, 0.0f,
         };
 
-        float squareCoords[] = {
-                0.375f,  0.6f, 0.0f,   // top left
-                0.375f, -0.6f, 0.0f,   // bottom left
-                -0.375f, -0.6f, 0.0f,   // bottom right
-                -0.375f,  0.6f, 0.0f }; // top right
+        squareCoords = sc;
 
         this.width = squareCoords[0] - squareCoords[6];
         this.height = squareCoords[1] - squareCoords[4];

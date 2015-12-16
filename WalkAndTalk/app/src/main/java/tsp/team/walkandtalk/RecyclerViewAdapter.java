2c75058f937ad12dc09bd2fd1c3c6e1a18381e0f
DisneyLover <stace.v.14@gmail.com>
2015-12-16 17:52:18 -0500

    Added comments to some files I did

--- WalkAndTalk/app/src/main/java/tsp/team/walkandtalk/RecyclerViewAdapter.java
+++ WalkAndTalk/app/src/main/java/tsp/team/walkandtalk/RecyclerViewAdapter.java
@@ -13,18 +13,35 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 /**
- * The RecyclerViewAdapter is the view that we use to choose characters and stages and do so by
- * scrolling left or right to view more options.
+ * The RecyclerViewAdapter is the custom adapter for the RecyclerViewer,
+ * used to create a horizontal listview for selecting characters and
+ * scenes, by scrolling left and right instead of up and down.
  */
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
-    private TypedArray images;
-    private TypedArray names;
-    private Activity activity;
+    private TypedArray images; // ResourceIDs of the character/scene images
+    private TypedArray names;  // Strings of the character/scene names
+    private Activity activity; // CharacterActivity or SceneActivity
     private Typeface font;
 
-    // Provide a reference to the views for each data item
-    // Complex data items may need more than one view per item, and
-    // you provide access to all the views for a data item in a view holder
+    /**
+     * Constructor for our custom RecyclerViewAdapter.
+     *
+     * @param imgs TypedArray of ResourceIDs of the character/scene images
+     * @param n    TyedArray of strings of the character/scene names
+     * @param a    Activity that created the RecyclerViewAdapter
+     * @param f    Font to use for the names
+     */
+    public RecyclerViewAdapter(TypedArray imgs, TypedArray n, Activity a, Typeface f) {
+        images = imgs;
+        names = n;
+        activity = a;
+        font = f;
+    } // Constructor
+
+    /**
+     * ViewHolder to hold the different view items included in te RecyclerView, and
+     * to set the onClick listener for the ImageView.
+     */
     public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         public IMyViewHolderClicks mListener;
         public ImageView mImageView;
@@ -48,25 +65,20 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         public static interface IMyViewHolderClicks {
             public void onClick(ImageView callerImage);
         }
-    }
+    } // ViewHolder
 
-    // Provide a suitable constructor (depends on the kind of dataset)
-    public RecyclerViewAdapter(TypedArray imgs, TypedArray n, Activity a, Typeface f) {
-        images = imgs;
-        names = n;
-        activity = a;
-        font = f;
-    }
-
-    // Create new views (invoked by the layout manager)
+    /**
+     * Create new view for the RecyclerView (invoked by the layout manager).
+     * @param parent
+     * @param viewType
+     * @return ViewHolder to hold the new RecyclerView
+     */
     @Override
     public RecyclerViewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
                                                              int viewType) {
-        // create a new view
+        // Create a new view
         View v = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.list_recycler_view, parent, false);
-        // set the view's size, margins, paddings and layout parameters
-        //...
 
         ViewHolder vh = new ViewHolder(v, new ViewHolder.IMyViewHolderClicks() {
             public void onClick(ImageView callerImage) {
@@ -102,8 +114,16 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         });
 
         return vh;
-    }
+    } // onCreateViewHolder
 
+    /**
+     * Create an integer array of the enemy image ResourceIDs based on the
+     * passed id of the TypedArray and the passed scene position.
+     * @param r         Resources
+     * @param array     ID of the TypedArray
+     * @param position  Position of the corresponding scene enemy array
+     * @return          Int array of enemy image ResourceIDs for proper scene
+     */
     private int[] getEnemyIds(Resources r, int array, int position){
         int id = r.obtainTypedArray(array).getResourceId(position, -1);
         TypedArray enemies = r.obtainTypedArray(id);
@@ -113,22 +133,30 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         }
         enemies.recycle();
         return enemy_ids;
-    }
+    } // getEnemyIds
 
-    // Replace the contents of a view (invoked by the layout manager)
+    /**
+     * Replaces contents of the view with the proper image/name for this position
+     * (invoked by the layout manager).
+     * @param holder    ViewHolder of our view elements
+     * @param position  Integer of current position in the list to populate view with
+     */
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        // - get element from your dataset at this position
-        // - replace the contents of the view with that element
         holder.mImageView.setImageResource(images.getResourceId(position, -1));
         holder.mImageView.setTag(position);
         holder.mTextView.setTypeface(font);
         holder.mTextView.setText(names.getString(position));
-    }
+    } // onBindViewHolder
 
-    // Return the size of your dataset (invoked by the layout manager)
+    /**
+     * Return the size of the dataset (number of images), as invoked by the
+     * layout manager.
+     * @return  integer size of our RecyclerView list
+     */
     @Override
     public int getItemCount() {
         return images.length();
-    }
-}
+    } // getItemCount
+    
+} // RecyclerViewAdapter
